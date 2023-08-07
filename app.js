6c31a8eae45409d754df1bcbe1c1d1a81ae2935f
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-08-07 16:26:12 -0300

    add login register and profile

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
-
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+app.get('/profile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'profile.html'));
+});
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el http://localhost:${PORT}`);
 });
